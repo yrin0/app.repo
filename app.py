ac5d271be8ec6404729aa811d21e81496f964ed1
yrin0 <joyelin0923@naver.com>
2024-10-26 18:22:10 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Sales Dashboard")
+
+st.write("Total Sales: ", total_sales)
+st.write("Average Transaction: ", avg_transaction)
+st.write("Profit Margin: ", profit_margin)  
+
