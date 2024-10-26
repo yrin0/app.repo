287dc1955d547b629679094b2d735a6cbab8b489
yrin0 <joyelin0923@naver.com>
2024-10-26 18:43:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+total_sales = 2296957.7003
+avg_transaction = 229.85666969878918
+profit_margin = 12.465361188958937
+
 st.title("Sales Dashboard")
 
 st.write("Total Sales: ", total_sales)
